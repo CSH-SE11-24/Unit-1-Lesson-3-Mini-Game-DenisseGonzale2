79d1d5387740a4e8e99c2a27d263fca6161cfe48
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:26:03 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function decision(option1, option2){
+if (decision == option1){
+	let option1= "You slowly walked in as the door creeked open";
+	console.log(option1)
+}else if(decision ==option2){
+	let option2 = "You go home and sleep. THE END!";
+	console.log(option2)
+}
+}
+let question= prompt("You see a haunted house, do you wish to enter? YES or NO?")
+decision (option1)
+//i dont know what to do
